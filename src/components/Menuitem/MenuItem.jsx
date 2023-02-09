436e72daea06d66a3811fddff6be186073204369
Aymen-TM <t_mustapha_aymen@outlook.com>
2023-02-09 22:30:34 +0100

    add menu section

--- src/components/Menuitem/MenuItem.jsx
+++ src/components/Menuitem/MenuItem.jsx
@@ -2,9 +2,20 @@ import React from 'react';
 
 import './MenuItem.css';
 
-const MenuItem = () => (
-  <div>
-    MenuItem
+const MenuItem = ({data}) => (
+  <div className='app__menuitem'>
+    <div className='app__meniitem-head'>
+      <div className='app__menuitem-name'>
+        <p className='p__cormorant' style={{color:"#DCCA87"}}>{data.title}</p>
+      </div>
+      <div className='app__menuitem-dash' />
+      <div className='app__menuitem-price'>
+        <p className='p__cormorant'>{data.price}</p>
+      </div>
+    </div>
+    <div className='app__menuitem-sub'>
+      <p className='p__opensans' style={{color:"#AAAAAA"}}>{data.tags}</p>
+    </div>
   </div>
 );
 
--- src/container/Menu/SpecialMenu.jsx
+++ src/container/Menu/SpecialMenu.jsx
@@ -1,10 +1,38 @@
 import React from 'react';
 
 import './SpecialMenu.css';
+import { SubHeading,MenuItem } from '../../components';
+import { images,data} from '../../constants';
 
 const SpecialMenu = () => (
-  <div>
-    SpecialMenu
+  <div className='app__specialmenu section__padding flex__center' id='#menu'>
+    <div className='app__specialmenu-title'>
+      <SubHeading title={"Menu that fits you palatte"} />
+      <h2 className='headtext__cormorant'>Today's Special</h2>
+    </div>
+    <div className='app__specialmenu-menu'>
+      <div className='app__specialmenu-menu_wine'> 
+        <p className='app__specialmenu-menu_heading'>Wine & Beer</p>
+        <div className='app__specialmenu_menu_items'> 
+        {data.wines.map((data)=>(
+          <MenuItem key={data.title} data={data} />
+        ))}
+        </div>
+      </div>
+      <div className='app__specialmenu_menu-img'>
+        <img src={images.menu} alt="menu" />
+      </div>
+      <div className='app__specialmenu-menu_cocktails'> 
+        <p className='app__specialmenu-menu_heading'>Cocktails</p>
+        <div className='app__specialmenu_menu_items'> 
+        {data.cocktails.map((data)=>(
+          <MenuItem key={data.title} data={data} />
+        ))}
+        </div>
+      </div>
+
+    </div>
+    <button className='custom__button' style={{marginTop:"4rem"}}>View More</button>
   </div>
 );
 
