4d57c9594d9147eb7f113fc9a51e495944209ece
Aymen-TM <t_mustapha_aymen@outlook.com>
2023-02-10 15:34:28 +0100

    add awards section

---
+++ src/components/AwardItem/AwardItem.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+import './AwardItem.css'
+
+const AwardItem = ({data}) => {
+  return (
+    <div className='app__awarditem'>
+        <div className='app__awarditem-img'>
+            <img src={data.imgUrl} alt="award" />
+        </div>
+        <div className='app__awarditem-content'>
+            <h5 className='p__cormorant' style={{color:"#DCCA87"}}>{data.title}</h5>
+            <p className='p__opensans'>{data.subtitle}</p>
+        </div>
+    </div>
+  )
+}
+
+export default AwardItem
--- src/container/Laurels/Laurels.jsx
+++ src/container/Laurels/Laurels.jsx
@@ -1,10 +1,24 @@
 import React from 'react';
 
 import './Laurels.css';
+import { images ,data} from '../../constants';
+import { SubHeading } from '../../components';
+import AwardItem from '../../components/AwardItem/AwardItem';
 
 const Laurels = () => (
-  <div>
-    Laurels
+  <div className='app__laurels app__wrapper app__bg section__padding '>
+      <div className='app__wrapper_info'>
+        <SubHeading title={"Awards & recognition"} />
+        <h1 className='headtext__cormorant'>Our Laurels</h1>
+        <div className='app__awards'>
+          {
+            data.awards.map((data)=><AwardItem data={data} />)
+          }
+        </div>
+      </div>
+      <div className='app__wrapper_img'>
+        <img src={images.laurels} alt="welcome" />
+      </div>
   </div>
 );
 
